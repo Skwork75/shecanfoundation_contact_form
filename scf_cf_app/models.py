0850e2781b9c32164738b1aef3e0d6f127abd67d
Saurabh Kumar <saurabhwork75@gmail.com>
2026-07-14 11:45:04 +0530

    updated models.py for admin

--- scf_cf_app/models.py
+++ scf_cf_app/models.py
@@ -4,7 +4,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     message = models.TextField()
-    submitted_at = models.DateTimeField(auto_now_add=True)
+    submitted_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.name
